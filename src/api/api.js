97fa080d31cb3e632d932881447bd427090f31ea
Xposean <843095666@qq.com>
2018-01-30 17:35:22 +0800

    同步代码

--- src/api/api.js
+++ src/api/api.js
@@ -1,5 +1,4 @@
 import wepy from 'wepy';
-import Toast from 'wepy-com-toast';
 const wxRequest = async (params = {}, url) => {
   wepy.showToast({
     title: '加载中',
@@ -15,9 +14,12 @@ const wxRequest = async (params = {}, url) => {
   wepy.hideToast();
   return res;
 };
-const getInterface = (params) => wxRequest(params, 'https://dict-mobile.iciba.com/interface/index.php?c=word&m=getsuggest&nums=10&client=6&is_need_mean=1&word=h');
-const getWordMean = (params) => wxRequest(params, 'https://www.iciba.com/index.php?a=getWordMean&c=search&list=1%2C2%2C3%2C4%2C5%2C8%2C9%2C10%2C12%2C13%2C14%2C18%2C21%2C22%2C3003%2C3005&word=hello');
-const getTranslate = (params) => wxRequest(params, 'https://fy.iciba.com/ajax.php?a=fy&f=auto&t=auto&w=hello%20world');
+//联想
+const getInterface = (params) => wxRequest(params, `https://dict-mobile.iciba.com/interface/index.php?c=word&m=getsuggest&nums=5&client=6&is_need_mean=1&word=${params.word}`);
+//释义
+const getWordMean = (params) => wxRequest(params, `https://www.iciba.com/index.php?a=getWordMean&c=search&list=1,3,4,5,8,9,10,12,13,14,18,21,3003,3005&word=${params.word}`);
+//翻译
+const getTranslate = (params) => wxRequest(params, `https://fy.iciba.com/ajax.php?a=fy&f=auto&t=auto&w=${params.content}`);
 
 
 module.exports = {
