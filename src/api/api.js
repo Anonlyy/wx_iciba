29d6d012e85fdf8b23a5ce6f1105c0cf5c56c94c
Xposean <qq843095666@gmaill>
2018-02-01 23:21:18 +0800

    增加释义详情显示内容

--- src/api/api.js
+++ src/api/api.js
@@ -16,8 +16,8 @@ const wxRequest = async (params = {}, url) => {
 };
 //联想
 const getInterface = (params) => wxRequest(params, `https://dict-mobile.iciba.com/interface/index.php?c=word&m=getsuggest&nums=5&client=6&is_need_mean=1&word=${params.word}`);
-//释义
-const getWordMean = (params) => wxRequest(params, `https://www.iciba.com/index.php?a=getWordMean&c=search&list=1,3,4,5,8,9,10,12,13,14,18,21,3003,3005&word=${params.word}`);
+//释义（基础释义,英英词典,双语例句,网络释义,同义词,反义词,词组搭配,例句）
+const getWordMean = (params) => wxRequest(params, `https://www.iciba.com/index.php?a=getWordMean&c=search&list=1,4,8,9,12,13,14,3005&word=${params.word}`);
 //翻译
 const getTranslate = (params) => wxRequest(params, `https://fy.iciba.com/ajax.php?a=fy&f=auto&t=auto&w=${params.content}`);
 
