72bcf1a4cf607efbbe2e3cb78e301e30e8f5d3c1
Xposean <qq843095666@gmaill>
2018-03-06 23:50:47 +0800

    增加选择翻译语言的功能

---
+++ readme.md
@@ -0,0 +1,57 @@
+##  wechat-weapp-iciba
+
+>微信小程序基于wepy框架开发-金山词霸
+
+### 项目使用
+- 克隆本项目
+- 进入目录安装依赖包
+- 启动服务
+	git clone https://github.com/Anonlyy/wx_iciba.git
+	cd wx_iciba
+	npm install
+	npm start
+####命令说明
+	// 开发阶段，监听src目录下文件变动情况，即使编译并复制到dist文件夹
+	npm start 
+	// 打包编译项目
+	npm run build
+
+
+
+### 感谢与支持
+**金山词典API**:https://github.com/jokermonn/-Api/blob/master/KingsoftDic.md
+**有道智云翻译 API**:http://ai.youdao.com/docs/doc-trans-api.s#p01
+**百度文字识别OCR API**:https://cloud.baidu.com/doc/OCR/OCR-API.html#.E8.AF.B7.E6.B1.82.E8.AF.B4.E6.98.8E
+**每日一句API**:http://open.iciba.com/?c=api
+
+---
+
+### 开发中的功能
+- 每日一句
+- 用户上传图片翻译功能
+- 界面优化
+
+---
+
+### 更新日志
+
+-  Jan 30, 2018
+    - 完成主页基本样式布局
+    - 实现查词联想功能
+    - 完成翻译页面基本样式布局
+-  Feb 5, 2018
+    - 完成释义具体数据的渲染 
+    - 完成音频组件播放功能
+    - 增加折叠面板展开特效 
+	- 增加回到顶部组件,并增加动画
+- Feb 12, 2018
+	- 完善诸多细节,将主页部分代码进行抽离,形成各自的组件
+	- 增加再次修改单词请求释义的情况,并新增释义组件
+- Mar 2, 2018
+	- 翻译界面的进一步样式编写,以及翻译API更改(有道)
+	- 完成文本翻译功能
+- Mar 6, 2018
+	- 修复MD5加密问题导致的API请求参数错误的bug问题
+	- 全局增加请求错误和加载中的toast提示
+	- 增加选择源翻译语言和翻译后语言的功能 
+- 正在更新中....
--- src/utils/config.js
+++ src/utils/config.js
@@ -3,87 +3,95 @@
  */
 module.exports = {
     exchangeFilter: {
-        "word_pl":{
+        "word_pl": {
             value: '复数形式'
         },
-        "word_third":{
+        "word_third": {
             value: '第三人称单数'
         },
-        "word_past":{
+        "word_past": {
             value: '过去分词'
         },
-        "word_done":{
+        "word_done": {
             value: '现在分词'
         },
-        "word_ing":{
+        "word_ing": {
             value: '正在进行时'
         },
-        "word_er":{
+        "word_er": {
             value: '比较级'
         },
-        "word_est":{
+        "word_est": {
             value: '最高级'
         },
-        "word_prep":{
+        "word_prep": {
             value: '介词形式'
         },
-        "word_adv":{
+        "word_adv": {
             value: '副词形式'
         },
-        "word_verb":{
+        "word_verb": {
             value: '动词形式'
         },
-        "word_noun":{
+        "word_noun": {
             value: '名词形式'
         },
-        "word_adj":{
+        "word_adj": {
             value: '形容词形式'
         },
-        "word_conn":{
+        "word_conn": {
             value: '连词形式'
         },
     },
     //翻译语言 '中文', '日文', '英文', '韩文', '法文', '葡萄牙文', '西班牙文'
-    LanguageData:[
-      {
-        name:'中文',
-        key:'zh-CHS'
-      },
-      {
-        name:'日文',
-        key:'ja'
-      },
-      {
-        name:'英文',
-        key:'EN'
-      },
-      {
-        name:'韩文',
-        key:'zh-CHS'
-      },
+    LanguageData: [
+        {
+            name:'自动',
+            key:'Auto'
+        },
+        {
+            name: '英文',
+            key: 'EN'
+        },
+        {
+            name: '中文',
+            key: 'zh-CHS'
+        },
+        {
+            name: '日文',
+            key: 'ja'
+        },
+        {
+            name: '韩文',
+            key: 'ko'
+        },
 
-      {
-        name:'法文',
-        key:'zh-CHS'
-      },
-      {
-        name:'葡萄牙文',
-        key:'zh-CHS'
-      },
-      {
-        name:'西班牙文',
-        key:'zh-CHS'
-      }
+        {
+            name: '法文',
+            key: 'fr'
+        },
+        {
+            name: '俄文',
+            key: 'ru'
+        },
+        {
+            name: '葡萄牙文',
+            key: 'pt'
+        },
+        {
+            name: '西班牙文',
+            key: 'es'
+        }
     ],
-    MultiArray: [['中文', '日文', '英文', '韩文', '法文', '葡萄牙文', '西班牙文'], ['中文', '日文', '英文', '韩文', '法文', '葡萄牙文', '西班牙文']],
+    MultiArray: [[ '自动','英文', '中文', '日文', '韩文', '法文','俄文','葡萄牙文', '西班牙文'], ['自动','英文', '中文', '日文', '韩文', '法文','俄文', '葡萄牙文', '西班牙文']],
     //图像识别API
-    ImageAPIConfig:{
-      ApiKey:'gxlKucQNdc9byyrQsx7bEGEk',
-      SecretKey:'bELd59TesCh6spRcQYqpBfuPyUZt5SWy'
+    ImageAPIConfig: {
+        ApiKey: 'gxlKucQNdc9byyrQsx7bEGEk',
+        SecretKey: 'bELd59TesCh6spRcQYqpBfuPyUZt5SWy'
     },
     //有道翻译API
-    YoudaoTranslateConfig:{
-      appKey:"3891f2b7f04c2513",
-      SecretKey:'gC4tO8eGSyGx4ay4slrPnBIkwuZT5uvr'
+    YoudaoTranslateConfig: {
+        appKey: "3891f2b7f04c2513",
+        SecretKey: 'gC4tO8eGSyGx4ay4slrPnBIkwuZT5uvr'
     }
 }
